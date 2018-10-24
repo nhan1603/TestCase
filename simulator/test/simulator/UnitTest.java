37a3d76f868dc2d99f035ef758846c934ec8e161
nhan1603 <38678910+nhan1603@users.noreply.github.com>
2018-10-24 19:22:21 +0700

    Update UnitTest.java

--- simulator/test/simulator/UnitTest.java
+++ simulator/test/simulator/UnitTest.java
@@ -1,247 +1,202 @@
-package simulator;
-
+package test;
+import static org.loadui.testfx.GuiTest.find; 
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.ArrayList;
-import org.junit.jupiter.api.BeforeAll;
+//import static org.hamcrest.CoreMatchers.*;
+import javafx.scene.input.KeyCode; 
+import javafx.stage.Stage; 
+import org.junit.After; 
+import static org.testfx.api.FxAssert.verifyThat; 
+import static org.junit.Assert.assertTrue; 
+import static org.loadui.testfx.Assertions.assertNodeExists; 
+import static org.testfx.matcher.base.NodeMatchers.*;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.loadui.testfx.GuiTest;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationTest;
 
+import gui.DataUtils;
+import gui.Main;
 import interfaces.AbstractComponent;
-//import junit.framework.Assert;
-import vgu.consumer.ConsumerFactory;
-import vgu.control.Control;
-import vgu.generator.GeneratorFactory;
-//
-class UnitTest {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseButton;
 
-	@BeforeAll
-	static void initialize() {
-		ConsumerFactory.setRunBehaviour(new double[] {1,1,1,1,1,1,1,1,1,1,1,1});		
+public class GuiTest1 extends ApplicationTest   {
+	@Override
+	public void start (Stage stage) throws Exception {
+	  Parent mainNode = FXMLLoader.load(Main.class.getResource("MainView.fxml"));
+	  stage.setScene(new Scene(mainNode));
+	  stage.show();
+	  stage.toFront();
 	}
 	
-	@Test
-	/**
-	 *register a 100W Device
-	 *register a 90W Generator (minimum and maximum supply)
-	 *I0: Check Frequency equals 49Hz
-	 */
-	void testLowFrequency() {
-		AbstractComponent c1 = ConsumerFactory.generate("c1", 100, 100, 100, 100);		
-		AbstractComponent g1 = GeneratorFactory.generate("g1", 90, 90, 90, 90);		
-		Control control = new Control();
-		control.addConsumer(c1);
-		control.addGenerator(g1);
-		double frequency = control.getFrequency();
-		System.out.println("T1: Frequency: "+frequency+"Hz");
-		assertEquals(49, frequency);
+	@Before
+	public void setUp () throws Exception {
 	}
-	@Test
-	/**
-	 *register a 90W Device
-	 *register a 100W Generator (minimum and maximum supply)
-	 *I0: Check Frequency equals 51Hz
-	 */
-	void testHighFrequency() {
-		AbstractComponent g1 = GeneratorFactory.generate("g1", 100, 100, 100, 100);		
-		AbstractComponent c1 = ConsumerFactory.generate("c1", 90, 90, 90, 90);		
-		Control control = new Control();
-		control.addConsumer(c1);
-		control.addGenerator(g1);
-		double frequency = control.getFrequency();
-		System.out.println("T2: Frequency: "+frequency+"Hz");
-		assertEquals(51, frequency);
-	}
-	@Test
-	/**
-	- register two 90W Device
-	- register two 100W Generator (minimum and maximum supply)
-	- I0: Check total demand equals 180W and total supply equal 200W
-	*/
-	void testDemandSupplyComputation() {
-		AbstractComponent g1 = GeneratorFactory.generate("g1", 100, 100, 100, 100);		
-		AbstractComponent c1 = ConsumerFactory.generate("c1", 90, 90, 90, 90);
-		AbstractComponent g2 = GeneratorFactory.generate("g2", 100, 100, 100, 100);		
-		AbstractComponent c2 = ConsumerFactory.generate("c2", 90, 90, 90, 90);
-		Control control = new Control();
-		control.addConsumer(c1);
-		control.addGenerator(g1);
-		control.addConsumer(c2);
-		control.addGenerator(g2);
-		double demand = control.getTotalDemand();
-		double supply = control.getTotalSupply();
-		System.out.println("T3: Demand: "+demand+"W Supply: "+supply+"W");
-		assertEquals(180, demand);
-		assertEquals(200, supply);
-	}
-	@Test
-//	- register a 100W Device
-//	- register a Generator with 90W, minimum 90W, maximum 100W, max-change 5W
-//	- I0: check supply is 90W
-//	- I1: check supply > previous supply
-//	- I2: check supply > previous supply
-	void testSupplyAdaption() {
 
-		AbstractComponent c1 = ConsumerFactory.generate("c1", 100, 100, 100, 100);		
-		AbstractComponent g1 = GeneratorFactory.generate("g1", 100, 90, 5, 5);
-		Control control = new Control();
-		control.addConsumer(c1);
-		control.addGenerator(g1);				
-		double supply1 = control.getTotalSupply();		
-		assertEquals(90, supply1);
-		control.nextIteration();
-		double supply2 = control.getTotalSupply();		
-		assertTrue(supply2>90);
-		control.nextIteration();
-		double supply3 = control.getTotalSupply();		
-		assertTrue(supply3>supply2);		
-		System.out.println("T4: Supply I0 "+supply1+"W Supply I1 "+supply2+"W Supply I2 "+supply3+"W");		
+	@After
+	public void tearDown () throws Exception {
+	  FxToolkit.hideStage();
+	  release(new KeyCode[]{});
+	  release(new MouseButton[]{});
 	}
-	
-	
-	
+
 	@Test
-	void testControlledBlackout() {
-//	- register 100 individual 1W Device
-//	- register a 70W Generator (minimum and maximum supply)
-//	- I0: Check Frequency is 47Hz, demand 100W, supply 70W -> 15 Devices will be unregistered
-//	- I1: Check Frequency is ~48.7Hz, demand 85W, supply 70W -> 13 Devices will be unregistred 
-//	- I2: Check frequency is ~49.8Hz demand 72W, supply 70W
-		Control control = new Control();
-		
-		ArrayList<AbstractComponent> consumer;
-		
-		consumer = ConsumerFactory.generate(100, 1, 0);		
-		for(AbstractComponent a : consumer) {			
-			control.addConsumer(a);
-		}
-		
-		AbstractComponent g1 = GeneratorFactory.generate("g1", 70, 70, 70, 70);		
-		control.addGenerator(g1);				
-		double f1 = control.getFrequency();		
-		int count1 = control.getConsumers().size();
-		assertEquals(100,count1);
-		
-		control.nextIteration();		
-		double f2 = control.getFrequency();
-		int count2 = control.getConsumers().size();
-		assertEquals(90, count2);
-		
-		control.nextIteration();		
-		double f3 = control.getFrequency();	
-		int count3 = control.getConsumers().size();
-		assertEquals(81, count3);
-		
-		System.out.println("T5: I0 (Frequency: "+f1+"Hz Consumers:"+count1+") I1 (Frequency: "+f2+"Hz Consumers:"+count2+") I2 (Frequency: "+f3+"Hz Consumers:"+count3+")");
+	public void testAddConsumerclicknon() {
+		Button button = (Button)GuiTest.find("#submitConsumer");
+		verifyThat(button,isVisible());
+		verifyThat(button,isEnabled());
+		clickOn("#submitConsumer");
 	}
 	@Test
-	void testGeneratorTurnOff() {
-//	- register a 100W Device
-//	- register a 1000W Generator (minimum and maximum supply)
-//	- I1: Check Generator will unregister
-		AbstractComponent c1 =  ConsumerFactory.generate("c1", 100, 100, 100, 100);		
-		AbstractComponent g1 = GeneratorFactory.generate("g1", 1000, 1000, 1000, 1000);		
-				
-		Control control = new Control();
-		control.addConsumer(c1);
-		control.addGenerator(g1);
-		int count1 = control.getGenerators().size();
-		control.nextIteration();		
-		int count2 = control.getGenerators().size();
-				
-		System.out.println("T7: I0 (Generators:"+count1+") I1 (Generators:"+count2+")");
-		assertEquals(count2, 0);
+	public void testAddGeneratorclicknon() {
+		Button button2 = (Button)GuiTest.find("#submitGenerator");
+		verifyThat(button2,isVisible());
+		verifyThat(button2,isEnabled());
+		clickOn("#submitGenerator");
 	}
-	@Test
-	void testOverDemand() {
-//	- register 100 individual 1W Device
-//	- register a 10W Generator (minimum and maximum supply)
-//	- I3: Check all Generator and devices will unregister
-		Control control = new Control();
-		ArrayList<AbstractComponent> consumer;
-		consumer = ConsumerFactory.generate(100, 1, 0);		
-		for(AbstractComponent a : consumer) {			
-			control.addConsumer(a);
-		}
-		AbstractComponent g1 = GeneratorFactory.generate("g1", 10, 10, 10, 10);		
-		control.addGenerator(g1);				
-		//double f1 = control.getFrequency();	
-		int count1 = control.getConsumers().size();
-		assertEquals(100,count1);
-		
-		control.nextIteration();		
-		//double f2 = control.getFrequency();
-		int count2 = control.getConsumers().size();
-		assertEquals(90, count2);
-		
-		control.nextIteration();		
-		//double f3 = control.getFrequency();	
-		int count3 = control.getConsumers().size();
-		assertEquals(81, count3);
-		control.nextIteration();
-		int count4 = control.getConsumers().size();
-		assertEquals(count4, 0);
-		System.out.println("T8: I0 (Consumers:"+count1+") I1 (Consumers:"+count2+") I2 (Consumers:"+count3+") I3 (Consumers:"+count4+")");
-	}
-	@Test
-	void testOverSupply() {
-//	- register a 10W Device
-//	- register 100 individual 1W Generator (minimum and maximum supply)
-//	- I3: Check all Generators and Devices will unregister
-		Control control = new Control();
-		AbstractComponent g1;
-		for(int i = 0; i < 100; i++) {
-			g1 = GeneratorFactory.generate("g"+i, 1, 1, 1, 1);
-			control.addGenerator(g1);
-		}
-		AbstractComponent c1 = ConsumerFactory.generate("c1", 10, 10, 10, 10);		
-		control.addConsumer(c1);				
-		//double f1 = control.getFrequency();	
-		int count1 = control.getGenerators().size();
-		assertEquals(100,count1);
-		
-		control.nextIteration();		
-		//double f2 = control.getFrequency();
-		int count2 = control.getGenerators().size();
-		assertEquals(85, count2);
-		
-		control.nextIteration();		
-		//double f3 = control.getFrequency();	
-		int count3 = control.getGenerators().size();
-		assertEquals(72, count3);
-		control.nextIteration();
-		int count4 = control.getGenerators().size();
-		assertEquals(count4, 0);
-		System.out.println("T9: I0 (Generators:"+count1+") I1 (Generators:"+count2+") I2 (Generators:"+count3+") I3 (Generators:"+count4+")");
-	}
-	
-	@Test
-	/**
-	 *register a 100W Device
-	 *register a 90W Generator (minimum and maximum supply)
-	 *I0: Check Frequency equals 49Hz
-	 */
-	void testNull() {
-		AbstractComponent c1 = ConsumerFactory.generate("c1", 100, 100, 100, 100);		
-		AbstractComponent g1 = GeneratorFactory.generate("g1", 90, 90, 90, 90);		
-		Control control= null;
-		assertThrows(NullPointerException.class,
-		            ()->{
-		            	control.addConsumer(c1);
-		        		control.addGenerator(g1);
-		            });
-	}
-	@Test
-	void testNull2() {
-		AbstractComponent c1 = null;
-		AbstractComponent g1 = null;
-		Control control= new Control();
-		assertThrows(NullPointerException.class,
-		            ()->{
-		            	control.addConsumer(c1);
-		            });
-		assertThrows(NullPointerException.class,
+	/*@Test
+	public void testAddGeneratorclicknon() {
+		assertThrows(RuntimeException.class,
 	            ()->{
-	        		control.addGenerator(g1);
+	            	clickOn("#submitGenerator");
 	            });
 	}
-	
+	/*@Test
+	public void testResultClicknon() {
+		assertThrows(LoadException.class,
+	            ()->{
+	            	clickOn("resultButton");
+	            });
+	}*/
+	@Test
+	public void testIteration() {
+		Button button2 = (Button)GuiTest.find("#resultButton");
+		verifyThat(button2,isVisible());
+		verifyThat(button2,isEnabled());
+		clickOn("#resultButton");
+		Button button3 = (Button)GuiTest.find("#nextButton");
+		verifyThat(button3,isVisible());
+		verifyThat(button3,isEnabled());
+		clickOn("#nextButton");
+		Button button4 = (Button)GuiTest.find("#showChartButton");
+		verifyThat(button4,isVisible());
+		verifyThat(button4,isEnabled());
+		clickOn("#showChartButton");
+	}
+	@Test
+	public void consumerbuttontest() {
+		Button button1 = (Button)GuiTest.find("#submitConsumer");
+		assertTrue(button1.isVisible());
+		verifyThat(button1,isEnabled());
+		verifyThat("#consumerSize",isVisible());
+		verifyThat("#consumerSize",isEnabled());
+		clickOn("#consumerSize");
+		write("50");
+		verifyThat("#consumerAvgPower",isVisible());
+		verifyThat("#consumerAvgPower",isEnabled());
+		clickOn("#consumerAvgPower");
+		write("100");
+		verifyThat("#consumerStd",isVisible());
+		verifyThat("#consumerStd",isEnabled());
+		clickOn("#consumerStd");
+		write("0");
+		clickOn("#submitConsumer");
+		
+		TableView<AbstractComponent> table = find("#table");
+		/*try {
+			List<AbstractComponent> consumers = DataUtils.getConsumersFromCSV("consumers.csv");
+			table.getItems().addAll(consumers);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}*/
+		for(int i=0; i<table.getItems().size() ;i++) {
+			assertEquals(table.getItems().get(i).getMaxPower(),100);
+			assertEquals(table.getItems().get(i).getMinPower(),0);
+			assertEquals(table.getItems().get(i).getMaxChange(),100);
+			assertEquals(table.getItems().get(i).getMinChange(),0);
+		}
+		
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("ConsumersView.fxml"));
+		assertNotNull(loader);
+		Button button3 = (Button)GuiTest.find("#addButton");
+		verifyThat(button3,isVisible());
+		verifyThat(button3,isEnabled());
+		Button button4 = (Button)GuiTest.find("#deleteButton");
+		verifyThat(button4,isVisible());
+		verifyThat(button4,isEnabled());
+		Button button5 = (Button)GuiTest.find("#saveButton");
+		verifyThat(button5,isVisible());	
+		verifyThat(button5,isEnabled());
+		//clickOn("#saveButton");
+	}
+	@Test
+	public void generatorbuttontest() {
+		Button button2 = (Button)GuiTest.find("#submitGenerator");
+		assertTrue(button2.isVisible());
+		verifyThat(button2,isEnabled());
+		verifyThat("#generatorSize",isVisible());
+		verifyThat("#generatorSize",isEnabled());		
+		clickOn("#generatorSize");
+		write("100");
+		verifyThat("#generatorStartPower",isVisible());
+		verifyThat("#generatorStartPower",isEnabled());
+		clickOn("#generatorTotalPower");
+		write("5000");
+		verifyThat("#generatorStartPower",isVisible());
+		verifyThat("#generatorStartPower",isEnabled());
+		clickOn("#generatorStartPower");
+		write("100");
+		clickOn("#submitGenerator");
+		
+		
+		TableView<AbstractComponent> table2 = find("#table");
+		/*try {
+			List<AbstractComponent> generators = DataUtils.getConsumersFromCSV("generators.csv");
+			table2.getItems().addAll(generators);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		for (int i=0;i<table2.getItems().size();i++) {
+			System.out.println(i+" "+table2.getItems().get(i).getMaxPower());
+		}*/
+		assertEquals(table2.getItems().get(0).getMaxPower(),50.0);
+		assertEquals(table2.getItems().get(0).getMinPower(),0);
+		assertEquals(table2.getItems().get(0).getMaxChange(),50.0);
+		assertEquals(table2.getItems().get(0).getMinChange(),0);
+		for(int i=1; i<table2.getItems().size();i++) {
+			assertEquals(table2.getItems().get(i).getMaxPower(),50.0);
+			assertEquals(table2.getItems().get(i).getMinPower(),0);
+			assertEquals(table2.getItems().get(i).getMaxChange(),25.0);
+			assertEquals(table2.getItems().get(i).getMinChange(),13.750000000000002);
+		}
+		/*assertEquals(table2.getItems().get(table2.getItems().size()/2).getMaxPower(),1250.0);
+		assertEquals(table2.getItems().get(table2.getItems().size()/2).getMinPower(),0);
+		assertEquals(table2.getItems().get(table2.getItems().size()/2).getMaxChange(),625.0);
+		assertEquals(table2.getItems().get(table2.getItems().size()/2).getMinChange(),625.0);
+		
+		for(int i=(table2.getItems().size()/2)+1; i<table2.getItems().size() ;i++) {
+			assertEquals(table2.getItems().get(i).getMaxPower(),25.510204081632654);
+			assertEquals(table2.getItems().get(i).getMinPower(),2.5510204081632653);
+			assertEquals(table2.getItems().get(i).getMaxChange(),19.13265306122449);
+			assertEquals(table2.getItems().get(i).getMinChange(),2.5510204081632653);
+		}*/
+		Button button6 = (Button)GuiTest.find("#addButton");
+		verifyThat(button6, isVisible());
+		verifyThat(button6,isEnabled());
+		Button button7 = (Button)GuiTest.find("#deleteButton");
+		verifyThat(button7,isVisible());	
+		verifyThat(button7,isEnabled());
+		Button button8 = (Button)GuiTest.find("#saveButton");
+		verifyThat(button8,isVisible());
+		verifyThat(button8,isEnabled());
+	}
 }
